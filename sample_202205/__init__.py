9c41b6d90be923d35d982e45c65e62a7963b7286
Teppei Nakano <teppei@iflab.tokyo>
2022-05-06 12:56:26 +0900

    add workflow

--- sample_202205/__init__.py
+++ sample_202205/__init__.py
@@ -1,1 +1,8 @@
 __version__ = '0.1.0'
+
+from operator import add
+from functools import reduce
+
+def sum(*args):
+    return reduce(add, args)
+
--- tests/test_sample_202205.py
+++ tests/test_sample_202205.py
@@ -1,5 +1,10 @@
 from sample_202205 import __version__
-
+from sample_202205 import sum
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_sum():
+    assert sum(1,2,3) == 1+2+3
+    assert sum(1,2,3,4,5) == 1+2+3+4+5
+    
