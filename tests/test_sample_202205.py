14c73d4814c72ab621a982084112ab82ff48f4c1
Teppei Nakano <teppei@iflab.tokyo>
2022-05-07 18:01:06 +0900

    support act

--- tests/test_sample_202205.py
+++ tests/test_sample_202205.py
@@ -8,3 +8,7 @@ def test_sum():
     assert sum(1,2,3) == 1+2+3
     assert sum(1,2,3,4,5) == 1+2+3+4+5
     
+def test_sum2():
+    assert sum(1,2,3) == 1+2+3
+    assert sum(1,2,3,4,5) == 1+2+3+4+5
+    
